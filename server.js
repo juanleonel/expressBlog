ddc995128ed63380866fdc23ca0e6130614a3791
Juan Albores <jlac_1993@outlook.com>
2023-12-03 17:35:29 -0500

    added the session handler

--- server.js
+++ server.js
@@ -1,43 +1,122 @@
 const express = require('express')
-const { engine } = require('express-handlebars')
+const { create } = require('express-handlebars')
+const weatherMiddleware = require('./src/libs/weather')
+const handlebars = require('handlebars')
+const handlers = require('./src/libs/handlers')
+const bodyParser = require('body-parser')
 
+const { credentials } = require('./src/config/config')
+
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const { doAuth } = require('./src/repository/dump.repository')
+const { isAuthenticated } = require('./src/libs/auth/authentication')
+
+const port = process.env.PORT || 3000
 const app = express()
 
-app.engine('handlebars', engine());
+app.use(cookieParser())
+app.use(session({
+  secret: 'Cat',
+  resave: false,
+  saveUninitialized: false,
+}))
+
+handlebars.registerHelper('helperUpperCase', function(value) {
+  return value.toUpperCase();
+})
+const hbs = create({
+  helpers: {
+    boo() { return 'BOOO!, como estas' },
+    section (name, options) {
+      if(!this._sections) this._sections = {}
+      this._sections[name] = options.fn(this)
+      return null
+    }
+  }
+})
+app.use(weatherMiddleware)
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.set('views', './src/views');
 app.use(express.static(__dirname + '/src/public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
-const port = process.env.PORT || 3000
+app.get('/', isAuthenticated, handlers.home)
+app.get('/about', isAuthenticated, handlers.about)
 
-app.get('/home', (req, res) => {
-  res.render('home')
+app.get('/login*', (req, res) => {
+  return res.render('auth/login')
+})
+app.get('/auth/login', (req, res) => {
+  return res.render('auth/login')
+})
+app.get('/auth/login-error', (req, res) => {
+  return res.render('auth/login-error')
 })
 
-app.get('/about', (req,res) => {
-  const fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple.",
-   ]
+app.post('/auth/process-login', async (req, res) => {
+  try {
+    const { email, password } = req.body
+    const result = await doAuth(email, password)
 
-   const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
-  res.render('about', { fortune: randomFortune })
+    if (!result) {
+      return res.format({
+        'text/html': () => res.redirect(303, '/auth/login-error')
+      })
+    }
+
+  //  return res.format({
+  //     // 'text/html': () => {
+  //     //   res.send('<b>hi there</b>');
+  //     // },
+  //     'application/json': () => {
+  //       res.json({ message: 'hi there' });
+  //     }
+  //   });
+
+    // Redirect to the view
+    req.session.user = result
+    return res.format({
+      'text/html': () => res.redirect(303, '/thank-you')
+    })
+  } catch (error) {
+    console.log(error)
+    return res.format({
+      'application/json': () => res.status(500).json({
+      error: 'error saving contact information' }),
+    })
+    // res.format({'text/plain': 'hi there',
+    // 'text/html': '<b>hi there</b>'});
+
+    // return res.format({
+    //   'text/html': () => res.redirect(303, 'auth/login-error'),
+    //   'application/json': () => res.status(500).json({
+    //   error: 'error saving contact information' }),
+    // })
+  }
 })
 
+app.get('/auth/logout', (req, res, next) => {
+  req.session.user = null
+
+  return res.render('auth/login')
+})
+
+app.get('/thank-you', isAuthenticated, (req, res) => {
+  if (req.session.user) {
+    const user = req.session.user
+    return res.render('thank-you', { user: user })
+  }
+
+  return res.render('auth/login')
+})
 
 // custom 404 page
-app.use((req, res) => {
-  res.status(404)
-  res.render('404')
-})
+// app.use(handlers.notFound)
+// // custom 500 page
+// app.use(handlers.serverError)
 
-// custom 500 page
-app.use((err, req, res, next) => {
-  res.status(500)
-  res.render('500')
+app.listen(port, () => {
+  console.log(`Express started on http://localhost:${port}; ` + `press Ctrl-C to terminate.`)
 })
-
-app.listen(port, () => console.log(`Express started on http://localhost:${port}; ` + `press Ctrl-C to terminate.`) )
---
+++ src/repository/dump.repository.js
@@ -0,0 +1,16 @@
+async function doAuth(userName, password) {
+  const defaultEmail = 'juan@correo.com'
+  const defaultPassword = '12343'
+  if (userName === defaultEmail && password === defaultPassword) {
+    return Promise.resolve({
+      userName,
+      password
+    })
+  }
+
+  return undefined
+}
+
+module.exports = {
+  doAuth
+}
